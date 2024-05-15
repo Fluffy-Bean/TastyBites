46b624954c6eb2a7bfb4648a1e0ec9cd0f1905c8
Fluffy <michal-gdula@protonmail.com>
2024-05-15 16:36:29 +0100

    Switch to tsconfig Fix changes due to change in target module

--- front/src/lib/api.ts
+++ front/src/lib/api.ts
@@ -3,14 +3,11 @@ import { type Item, type JSONResponse } from "./types";
 const API_URL = "http://127.0.0.1:8080";
 
 export async function getPopularToday(): Promise<Item[]> {
-    const response = await fetch(`${API_URL}/api/items`);
+    const response: Response = await fetch(`${API_URL}/api/items`);
     const { data, error }: JSONResponse = await response.json();
 
-    if (response.ok) {
-        if (data?.item) {
-            return data?.item;
-        }
-        throw new Error("Failed to fetch popular today");
-    }
-    throw new Error(error);
+    if (!response.ok) throw new Error(error);
+    if (!data?.item) throw new Error("Failed to fetch popular today");
+
+    return data?.item;
 }
--- front/src/lib/utils.js
+++
@@ -1,6 +0,0 @@
-export function expandOnTyping(element) {
-    element.oninput = (event) => {
-        event.target.style.height = ""; // skipcq: JS-W1032
-        event.target.style.height = `${event.target.scrollHeight + 2}px`;
-    };
-}
